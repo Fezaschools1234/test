6ad6688a4c141c860cf2932af61f42e431875bf1
Fezaschools1234 <tkitambazi@gmail.com>
2026-02-10 10:18:16 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // In a real scenario, you'd send this data to a server
+    alert('Logging in... (Wait, this is just a clone!)');
+});
